746957260aaf62fa46201f27d7c1d9f91b38369f
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-10-29 22:03:57 +0600

    templete file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="text-xl text-red-600 flex justify-center my-10 bg-teal-300">
+        React & tailwind css
+      </div>
+    </>
+  );
+};
+
+export default App;
